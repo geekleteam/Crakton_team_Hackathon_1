ad08ef7fec7b257b2aceb10a73fd0e8036a979a9
Ruchika30 <sood.ruchika@siesgst.ac.in>
2024-07-09 08:40:25 +0530

    fix: updated next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,25 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "export",
+	images: {
+		unoptimized: true
+	},
+	experimental: {
+		webpackBuildWorker: true
+	},
+	webpack(config) {
+		config.module.rules.push({
+			test: /\.svg$/i,
+			issuer: /\.[jt]sx?$/,
+			use: ["@svgr/webpack"]
+		})
+		return config
+	},
+	
+	typescript: {
+		
+		ignoreBuildErrors: true
+	}
+}
 
-export default nextConfig;
+module.exports = nextConfig
